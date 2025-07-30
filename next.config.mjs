f06d5b85a49cadbba188e820632a53a2af159b21
ronit21102-js <ronit.roushan@kreedalabs.com>
2025-07-30 15:19:36 +0530

    made cards for welcome page

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  transpilePackages: ["fuma-docs"],
 };
 
 export default withMDX(config);
